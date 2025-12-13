e7db189afaf317b09f46dbc3a65c50634073f587
Fatemeh Goudarzi <f-goudarzi-d@local.lan>
2025-12-13 14:02:04 +0330

    new

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: '/digikala/',
-
+  base: '/', 
 })
-
